9756747411c5a0e0e6b8841ba6657427586290b6
gustaveH <ghabiman@gmail.com>
2020-04-21 19:00:47 -0400

    Added example of a variable and method

---
+++ ruby_playground.rb
@@ -0,0 +1,20 @@
+puts "Hello World!"
+#\n puts forever on a new line
+print "Hello world \nforever!! \n" 
+
+#variable is a name we give to a a reference to some location in memory
+greeting = "Hello world!"
+puts greeting
+
+# ruby method
+def say_hello
+    puts "Hello World!!!!"
+end
+say_hello # calling a method
+
+# ruby method with argument
+def say_hello_wih_argument (name)
+    puts name
+end
+say_hello_wih_argument "How are You Gustave?"
+
